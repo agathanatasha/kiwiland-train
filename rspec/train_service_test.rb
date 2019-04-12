bed75d088f2104f78e7331e3b9706ab9f19ac763
Natasha Ho <natashaho1@gmail.com>
2019-04-11 22:13:33 -0400

    WIP get_possible_paths_count in train service

--- rspec/train_service_test.rb
+++ rspec/train_service_test.rb
@@ -1,38 +1,71 @@
-require '../train_service'
+require_relative '../train_service'
 describe 'trains' do
     NO_SUCH_ROUTE = 'NO SUCH ROUTE'
+    GRAPH = ['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']
 
     context 'get route distance' do
+        routing_service = Train_Service.new GRAPH
         it 'returns NO SUCH ROUTE when only partial route exists' do
             route = "AED"
-            expect(Train_Service.get_distance(route)).to eq(NO_SUCH_ROUTE)
+            expect(routing_service.get_distance(route)).to eq(NO_SUCH_ROUTE)
         end
 
         it 'returns NO SUCH ROUTE when none of the routes exists' do
             route = "ED"
-            expect(Train_Service.get_distance(route)).to eq(NO_SUCH_ROUTE)
+            expect(routing_service.get_distance(route)).to eq(NO_SUCH_ROUTE)
         end
 
         it 'returns the distance of the route with one stop route if the route exists' do
             route = "AD"
-            expect(Train_Service.get_distance(route)).to eq(5)
+            expect(routing_service.get_distance(route)).to eq(5)
         end
 
         it 'returns the sum of distance for a two-stop route if route exists' do
             route = "ABC"
-            expect(Train_Service.get_distance(route)).to eq(9)
+            expect(routing_service.get_distance(route)).to eq(9)
         end
 
         it 'returns the sum of distance for multiple-stop route if route exists' do
             route = "AEBCD"
-            expect(Train_Service.get_distance(route)).to eq(22)
+            expect(routing_service.get_distance(route)).to eq(22)
         end
     end
 
     context 'get number of possible paths between two towns' do
-        it 'returns number of paths available between two towns with max 1 stop' do
-            expect(Train_Service.get_possible_paths_count("A", "C", 1)).to eq(0)
+        it 'returns 0 when max_stop = 0' do
+            routing_service = Train_Service.new ["AE2", "BC3", "DA8", "AC4"]
+            expect(routing_service.get_possible_paths_count("A", "C", 0)).to eq(0)
         end
+
+        context 'maximum number of stops = 1' do
+            it 'returns 0 when no path exists' do
+                routing_service = Train_Service.new ["AE2", "BC3", "DA8"]
+                expect(routing_service.get_possible_paths_count("A", "C", 1)).to eq(0)
+            end
+
+            it 'returns 1 when one path exists' do
+                routing_service = Train_Service.new ["AE2", "BC3", "DA8", "AD5"]
+                expect(routing_service.get_possible_paths_count("A", "D", 1)).to eq(1)
+            end
+        end
+
+        context 'maximum number of stops = 2' do
+            it 'returns 0 when no paths exist' do
+                routing_service = Train_Service.new ["AB5", "AC2", "CD5", "DE2"]
+                expect(routing_service.get_possible_paths_count("A", "E", 2)).to eq(0)
+            end
+
+            it 'returns 1 when one 1-stop path exist' do
+                routing_service = Train_Service.new ["AB5", "AC2", "CD5", "DE2"]
+                expect(routing_service.get_possible_paths_count("A", "B", 2)).to eq(1)
+            end
+
+            # it 'returns 1 when one 2-stop path exist' do
+            #     routing_service = Train_Service.new ["AB5", "BC2", "CD5", "DE2"]
+            #     expect(routing_service.get_possible_paths_count("A", "C", 2)).to eq(1)
+            # end
+        end
+        # if origin and destination is the same
     end
     
 end
--- train_service.rb
+++ train_service.rb
@@ -1,24 +1,32 @@
+require_relative 'train_map'
+
 class Train_Service
     NO_SUCH_ROUTE = "NO SUCH ROUTE"
     GRAPH = ['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']
 
-    def self.get_distance route, graph=GRAPH
+    def initialize routes
+        @map = Train_Map.new
+        @map.generate_map(routes)
+    end
+
+    def get_distance route
         total_distance = 0
         for i in 0...route.length-1
-            journey = route[i] + route[i+1]
-            journey_distance = 0
-            for edge in graph
-                if edge.start_with? journey
-                    journey_distance = edge[-1].to_i
-                    total_distance += journey_distance
-                end
-            end
-            return NO_SUCH_ROUTE if journey_distance == 0
+            origin_city_name = route[i]
+            destination_city_name = route[i+1]
+            journey_distance = @map.nodes[origin_city_name].distance_to(destination_city_name)
+            return NO_SUCH_ROUTE if journey_distance == -1
+            total_distance += journey_distance
         end
         return total_distance
     end
 
-    def self.get_possible_paths_count origin, destination, max_stops, graph=GRAPH
-        
+    def get_possible_paths_count origin, destination, max_stops
+        possible_paths = []
+        if @map.nodes[origin].distance_to(destination) == -1 || max_stops == 0
+            return 0
+        else
+            return 1
+        end
     end
 end
